5b22b70a8a77e570e5b3ec6c11464688addd2046
d8815460 <d8815460@yahoo.com.tw>
2013-12-26 21:52:33 +0800

    init with Cartoon.

---
+++ Cartoon/Cartoon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Cartoon
+//
+//  Created by 駿逸 陳 on 2013/12/26.
+//  Copyright (c) 2013年 駿逸 陳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cartoon/Cartoon/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Cartoon
+//
+//  Created by 駿逸 陳 on 2013/12/26.
+//  Copyright (c) 2013年 駿逸 陳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
